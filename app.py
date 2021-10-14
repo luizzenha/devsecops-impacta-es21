670991748882f90bcfcb7c36358de8481ad368ec
luizzenha <lzenha@gmail.com>
2021-10-13 22:29:27 -0300

    Mensagem customizada

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Processo CI com Travis e Deploy no Heroku"
 
 if __name__ == '__main__':
     app.run(debug=True)
